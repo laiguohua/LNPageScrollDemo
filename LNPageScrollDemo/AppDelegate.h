ffc6e4da90fa50d10e1e3aa4fb46b9eadd91f0d4
laiguohua <469790503@qq.com>
2018-03-25 16:10:07 +0800

    Initial Commit

---
+++ LNPageScrollDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LNPageScrollDemo
+//
+//  Created by Mr.lai on 2018/3/25.
+//  Copyright © 2018年 Mr.lai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LNPageScrollDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LNPageScrollDemo
+//
+//  Created by Mr.lai on 2018/3/25.
+//  Copyright © 2018年 Mr.lai. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
